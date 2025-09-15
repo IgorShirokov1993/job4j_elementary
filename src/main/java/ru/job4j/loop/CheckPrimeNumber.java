3d8a39889bb312bac75d8cbd69f645b388c3fb50
IgorShirokov1993 <igorshirokov.1993@gmail.com>
2025-09-15 16:44:54 +0300

    The "if" statement has been simplified.

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,10 +2,7 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number <= 1) {
-            prime = false;
-        }
+        boolean prime = number > 1;
         for (int index = 2; index <= number - 1; index++) {
             if ((number % index) == 0) {
                 prime = false;
