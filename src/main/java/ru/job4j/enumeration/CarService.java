e5a53cdd3e99ac0e728067b16e5d5b066901f754
IgorShirokov1993 <igorshirokov.1993@gmail.com>
2026-04-07 21:51:08 +0300

    Enum - класс c ограниченным набором объектов или "класс-перечислений".

---
+++ src/main/java/ru/job4j/enumeration/CarService.java
@@ -0,0 +1,14 @@
+package ru.job4j.enumeration;
+
+public class CarService {
+    public static void main(String[] args) {
+        Status toyota = Status.FINISHED;
+        Status volvo = Status.WAITING;
+        System.out.println("Статус Toyota: " + toyota);
+        System.out.println("Статус Volvo: " + volvo);
+        Status[] statuses = Status.values();
+        for (Status s : statuses) {
+            System.out.println("Название статуса: " + s.name() + ", Порядковый номер статуса: " + s.ordinal());
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/enumeration/Status.java
@@ -0,0 +1,50 @@
+package ru.job4j.enumeration;
+
+public enum Status {
+    ACCEPTED("Принят") {
+        private String message = "Автомобиль принят на СТО";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+
+    },
+    IN_WORK("В работе") {
+        private String message = "Автомобиль в работе";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    },
+    WAITING("Ожидание") {
+        private String message = "Автомобиль ожидает запчасти";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    },
+    FINISHED("Работы завершены") {
+        private String message = "Все работы завершены";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    };
+
+    private String info;
+
+    Status(String info) {
+        this.info = info;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public abstract String getMessage();
+
+}
